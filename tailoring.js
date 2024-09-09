20c467bc7d7680bbf6cdf6077013ac590e4678a7
ragul <ragulaa098@gmail.com>
2024-09-09 20:48:39 +0530

    js update

---
+++ tailoring.js
@@ -0,0 +1,13 @@
+
+function sideo(){
+    const blockk=document.getElementById("sidee")
+    blockk.style.left=0}
+function sidec(){
+    const blockk=document.getElementById("sidee")
+    blockk.style.left="-60%"}
+
+    
+    
+
+    
+   
